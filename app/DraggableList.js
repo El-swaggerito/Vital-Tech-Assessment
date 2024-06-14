ee70b747112e366e20005fbb6fb988e768157639
Muhammad Zayyad Mukhtar <zayyadmukhtar@gmail.com>
2024-06-14 09:47:13 +0100

    Final Commit

--- app/DraggableList.js
+++ app/DraggableList.js
@@ -7,50 +7,50 @@ import image3 from "../public/images/image-3.png";
 import image4 from "../public/images/image-4.png";
 import image5 from "../public/images/image-5.png";
 import image6 from "../public/images/image-6.png";
-
-const locationEmoji = "📍";
+import location from "../public/images/location.png";
 
 const initialItems = [
   {
     id: "1",
     title: "Scotland Island",
-    location: `${locationEmoji} Sydney, Australia`,
+    location: "Sydney, Australia",
     imageUrl: image1,
   },
   {
     id: "2",
     title: "The Charles Grand Brasserie & Bar",
-    location: `${locationEmoji} Lorem ipsum, Dolor`,
+    location: "Lorem ipsum, Dolor",
     imageUrl: image2,
   },
   {
     id: "3",
     title: "Bridge Climb",
-    location: `${locationEmoji} Dolor, Sit amet`,
+    location: "Dolor, Sit amet",
     imageUrl: image3,
   },
   {
     id: "4",
     title: "Scotland Island",
-    location: `${locationEmoji} Sydney, Australia`,
+    location: "Sydney, Australia",
     imageUrl: image4,
   },
   {
     id: "5",
     title: "Clam Bar",
-    location: `${locationEmoji} Etcetera veni, Vidi vici`,
+    location: "Etcetera veni, Vidi vici",
     imageUrl: image5,
   },
   {
     id: "6",
     title: "Vivid Festival",
-    location: `${locationEmoji} Sydney, Australia`,
+    location: "Sydney, Australia",
     imageUrl: image6,
   },
 ];
 
 const DraggableList = () => {
   const [items, setItems] = useState(initialItems);
+  const [draggingId, setDraggingId] = useState(null);
 
   return (
     <Reorder.Group
@@ -63,13 +63,15 @@ const DraggableList = () => {
         <Reorder.Item
           key={item.id}
           value={item}
-          className="bg-white p-4 rounded-lg shadow-md"
+          className="bg-white p-4 rounded-lg"
+          onDragStart={() => setDraggingId(item.id)}
+          onDragEnd={() => setDraggingId(null)}
           whileDrag={{
+            cursor: "grabbing",
             scale: 0.5,
-            boxShadow: "0 10px 15px rgba(0, 0, 0, 0.2)",
           }}
         >
-          <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2">
             <img
               src={item.imageUrl.src}
               alt={item.title}
@@ -77,7 +79,16 @@ const DraggableList = () => {
             />
             <div>
               <h3 className="text-lg font-semibold">{item.title}</h3>
-              <p className="text-gray-500">{item.location}</p>
+              {draggingId !== item.id && (
+                <p className="text-gray-500">
+                  <img
+                    src={location.src}
+                    alt="Location Emoji"
+                    className="inline-block w-4 h-4 mr-1"
+                  />
+                  {item.location}
+                </p>
+              )}
             </div>
           </div>
         </Reorder.Item>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+"use client";
 import DraggableList from "../app/DraggableList";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-10">
-      <main className="max-w-xl mx-auto p-4">
+    <div className="min-h-screen py-10">
+      <main className="max-w-xl mx-auto p-4 ">
         <DraggableList />
       </main>
     </div>
